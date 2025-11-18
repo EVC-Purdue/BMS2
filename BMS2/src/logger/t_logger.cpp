92a6b3e7bdd47ded70e1539fc939deebc9e508e3
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-11-18 15:57:36 -0500

    std:visit (no enum match in C++?)

--- BMS2/src/logger/t_logger.cpp
+++ BMS2/src/logger/t_logger.cpp
@@ -1,7 +1,10 @@
 #include "freertos/FreeRTOS.h"
 
+#include "logger/q_logger.hpp"
+
 
 #include "logger/t_logger.hpp"
+
 
 
 namespace t_logger {
@@ -11,6 +14,22 @@ TLogger::TLogger(uint32_t period)
 
 void TLogger::task() {
 	// Implement logging functionality here
+	q_logger::Message msg = {};
+
+	// Read messages from the logger queue
+	if (xQueueReceive(q_logger::g_logger_queue, &msg, 0) == pdTRUE) {
+		std::visit([](auto&& arg) {
+			using T = std::decay_t<decltype(arg)>;
+			if constexpr (std::is_same_v<T, q_logger::msg::Write>) {
+				// Handle Write message
+			} else if constexpr (std::is_same_v<T, q_logger::msg::Read>) {
+				// Handle Read message
+			} else if constexpr (std::is_same_v<T, q_logger::msg::Flush>) {
+				// Handle Flush message
+			}
+		}, msg);
+	}
+	
 }
 
 
