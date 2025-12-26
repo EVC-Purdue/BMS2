87d49c15e865db975504e0c7beb9541d3813bece
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-12-26 15:38:52 -0800

    Moving fault enums to their own folder

--- BMS2/src/battery/t_battery.hpp
+++ BMS2/src/battery/t_battery.hpp
@@ -61,43 +61,6 @@ struct BatteryData {
 };
 
 
-// TODO: move faults into own header
-
-// Faults stay active until cleared
-// If the bit is set in the current or previous fault, it is considered active
-enum PersistentFault {
-	CELL_UNDERVOLTAGE = 0,
-	CELL_OVERVOLTAGE,
-	BATTERY_UNDERVOLTAGE,
-	BATTERY_OVERVOLTAGE,
-	BATTERY_VOLTAGE_IMBALANCE,
-	OVERCURRENT,
-	UNDERCURRENT,
-	TEMP_0,
-	TEMP_1,
-	TEMP_2,
-	TEMP_3,
-	PERSISTENT_FAULTS_END
-};
-
-// Faults that clear themselves
-// If the bit is set in the current fault, it is considered active
-enum LiveFault {
-	// No live faults defined yet
-	LIVE_FAULTS_END = PERSISTENT_FAULTS_END
-};
-
-// Faults that warn but do not take action
-enum WarningFault {
-	OVERPOWER = LIVE_FAULTS_END,
-	ANY_BYPASSED,
-	TEMPS_IMBALANCE,
-	WARNING_FAULTS_END
-};
-
-static_assert(WarningFault::WARNING_FAULTS_END <= 32, "Total fault count exceeds 32 bits");
-
-
 // PARAMETERS
 
 
