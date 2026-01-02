f538fab509fe943bbeeb32c2bd6c335497784135
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2026-01-02 14:01:43 -0800

    documentation for TLogger member variables

--- BMS2/src/logger/t_logger.hpp
+++ BMS2/src/logger/t_logger.hpp
@@ -30,13 +30,22 @@ constexpr size_t LOG_LINE_MAX_SIZE = 120; // Maximum size of a single log line. 
 
 class TLogger : public task_base::TaskBase {
 	private:
+        // If the SPIFFS usage is above the max ratio, whether to delete the log file to make space
+        // If false, new log data will be discarded when storage is full
         bool param_delete_log_if_full;
 
+        // Cached SPIFFS usage ratio to avoid frequent checks
         float spiffs_usage_ratio;
+        // Number of writes since last SPIFFS usage check
         size_t spiffs_usage_write_count;
         
+        // Current index in the write buffer
         size_t write_buffer_index;
+        // Buffer for accumulating log lines before writing to SPIFFS
+        // Allows batching multiple log lines into one write operation
         char write_buffer[WRITE_BUFFER_SIZE];
+        // Buffer for formatting a single log line. Must be large enough to hold one full log line
+        // as this is not checked.
         char log_line_buffer[LOG_LINE_MAX_SIZE];
 
         // Flushes the current write buffer to the SPIFFS log file and resets the buffer index.
